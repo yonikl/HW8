26e4def8d11bc443754f183e3ebc55b10368e776
yehuda pilent <pylent@g.jct.ac.il>
2022-05-20 13:54:49 +0300

    I added the Vectors stack

--- main.cpp
+++ main.cpp
@@ -6,18 +6,5 @@ using namespace std;
 
 int main() {
 
-      StackVector<int> a(50);
-
-      a.push(1);
-      a.push(2);
-      a.push(3);
-      a.push(4);
-
-    for (int i = 0; i < 4; ++i) {
-        cout << a.top() << endl;
-        a.pop() ;
-    }
-
-
     return 0;
 }
